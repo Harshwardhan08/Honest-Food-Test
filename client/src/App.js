5fbf3356aff0f3a2d96af767390cfa0403cbf617
Harshwardhan Chauhan <harshwardhan.chauhan@bold.com>
2020-11-29 17:28:32 +0530

    minor modifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
+import { CssBaseline, Container, TextField, Typography, Button } from '@material-ui/core';
 import fetch from 'isomorphic-fetch';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
